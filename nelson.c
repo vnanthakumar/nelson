3bfe53b9dd6d942819da7cf6dad9e15061ed3c47
nelsonphilip <35652437+nelsonphilip@users.noreply.github.com>
2018-01-21 16:08:06 +0530

    Create nelson.c

---
+++ nelson.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a;
+  printf("enter the a value");
+  scanf("%d",&a);
+  if(a>0)
+  {
+    printf("The value is possitive");
+  }
+  else if(a<0)
+  {
+  printf( "The value is negative");
+  }
+  else(a=o)
+  {
+    printf( "the value is zero");
+   } 
+} 
+   
