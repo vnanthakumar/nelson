e3dd2b37213edb5a86243f89871d72a76afeeb5f
nelsonphilip <35652437+nelsonphilip@users.noreply.github.com>
2018-01-30 16:05:34 +0530

    Create nelson1.c

---
+++ nelson1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<cinio.h>
+void main()
+{
+   int a ;
+   scanf( "%d",&a);
+   if(0>a)
+   {
+      printf(" positive");
+    }
+    else(0<a)
+   {
+     printf("negative");
+     }
+    getch();
+    }
